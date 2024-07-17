7d73c2f800fad64751bc0dbb51c76d56842644b5
ananyahjha93 <ananyahjha93@gmail.com>
2024-07-16 17:21:25 -0700

    run reference and baselines

---
+++ scripts/beaker/data_pruning/gantry-script.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -ex
+
+NUM_NODES=1
+TASK_NAMES=("reference/olmo-60M-pes2o-24B" "reference/olmo-60M-pes2o-60B")
+MAX_DURATIONS=("12_000" "30_000")
+
+length=${#MAX_DURATIONS[@]}
+
+for ((i=0; i < $length; i++)); do
+    gantry run \
+    --workspace ai2/data-pruning \
+    --task-name ${TASK_NAME} \
+    --description "data pruning experiments" \
+    --priority high \
+    --preemptible \
+    --beaker-image shanea/olmo-torch2.2-gantry \
+    --cluster ai2/jupiter-cirrascale-2 \
+    --gpus 8 \
+    --replicas "${NUM_NODES}" \
+    --leader-selection \
+    --host-networking \
+    --budget ai2/oe-training \
+    --no-nfs \
+    --propagate-failure \
+    --synchronized-start-timeout 30m \
+    --env LOG_FILTER_TYPE=local_rank0_only \
+    --env OMP_NUM_THREADS=8 \
+    --env OLMO_TASK=model \
+    --env-secret WANDB_API_KEY=ANANYA_WANDB_API_KEY \
+    --env-secret AWS_ACCESS_KEY_ID=ANANYA_AWS_ACCESS_KEY_ID \
+    --env-secret AWS_SECRET_ACCESS_KEY=ANANYA_AWS_SECRET_ACCESS_KEY \
+    --shared-memory 10GiB \
+    --venv base \
+    --yes \
+    --timeout=-1 \
+    -- /bin/bash -c "scripts/beaker/tiny/torchrun-script.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} ${TASK_NAMES[$i]} ${MAX_DURATIONS[$i]}"
+done
