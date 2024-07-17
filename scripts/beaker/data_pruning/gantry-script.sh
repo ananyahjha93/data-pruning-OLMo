8998add6bfa32c1845b63b4518d1549b5a87c199
ananyahjha93 <ananyahjha93@gmail.com>
2024-07-16 20:00:55 -0700

    300M pes2o baseline

--- scripts/beaker/data_pruning/gantry-script.sh
+++ scripts/beaker/data_pruning/gantry-script.sh
@@ -23,7 +23,6 @@ gantry run \
     --budget ai2/oe-training \
     --no-nfs \
     --propagate-failure \
-    --synchronized-start-timeout 30m \
     --env LOG_FILTER_TYPE=local_rank0_only \
     --env OMP_NUM_THREADS=8 \
     --env OLMO_TASK=model \
